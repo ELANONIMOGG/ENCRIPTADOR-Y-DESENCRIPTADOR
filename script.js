4abe83d5f6825e082b11cf41b585b73617f52eb6
Miguel Zamudio <94863947+ELANONIMOGG@users.noreply.github.com>
2023-01-23 21:48:18 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+function encriptar() {
+    let palabra = document.getElementById("inputEncriptar").value;
+    let palabraEncriptada = "";
+    for (let i = 0; i < palabra.length; i++) {
+        switch (palabra[i]) {
+            case "a":
+                palabraEncriptada += "ai";
+                break;
+            case "e":
+                palabraEncriptada += "enter";
+                break;
+            case "i":
+                palabraEncriptada += "imes";
+                break;
+            case "o":
+                palabraEncriptada += "ober";
+                break;
+            case "u":
+                palabraEncriptada += "ufat";
+                break;
+            default:
+                palabraEncriptada += palabra[i];
+                break;
+        }
+    }
+    document.getElementById("outputEncriptar").value = palabraEncriptada;
+}
+
+function desencriptar() {
+    let palabra = document.getElementById("inputDesencriptar").value;
+    let palabraDesencriptada = "";
+    let i = 0;
+    while (i < palabra.length) {
+        if (palabra.substring(i, i + 2) === "ai") {
+            palabraDesencriptada += "a";
+            i += 2;
+        } else if (palabra.substring(i, i + 5) === "enter") {
+            palabraDesencriptada += "e";
+            i += 5;
+        } else if (palabra.substring(i, i + 4) === "imes") {
+            palabraDesencriptada += "i";
+            i += 4;
+        } else if (palabra.substring(i, i + 4) === "ober") {
+            palabraDesencriptada += "o";
+            i += 4;
+        } else if (palabra.substring(i, i + 4) === "ufat") {
+            palabraDesencriptada += "u";
+            i += 4;
+        } else {
+            palabraDesencriptada += palabra[i];
+            i++;
+        }
+    }
+    document.getElementById("outputDesencriptar").value = palabraDesencriptada;
+}
+
+function copyTextEncriptado() {
+    let textoEncriptado = document.getElementById("outputEncriptar");
+    textoEncriptado.select();
+    document.execCommand('copy');
+}
+
+function copyTextDesencriptado() {
+    let textoDesencriptado = document.getElementById("outputDesencriptar");
+    textoDesencriptado.select();
+    document.execCommand('copy');
+}
